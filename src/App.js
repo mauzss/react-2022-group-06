2b42f19bac55984b02ae91aac88756f414a3e1dc
Marcos Gabriel Martinez <gabriel_martinez_2409@hotmail.es>
2022-05-14 22:20:12 -0300

    Creando el navbar incompleto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//Components
+import Navbar from './Components/Navbar/Navbar';
 
 //Styles
 import './GlobalStyles.css';
@@ -8,11 +12,21 @@ import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import Group from './Pages/Group';
 import SignupPage from './Pages/SignupPage';
+import PageNotFound from './Pages/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      hola mundo
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/group' element={<Group/>} />
+          <Route path='/contact' element={<Contact/>} />
+          <Route path='/signup' element={<SignupPage/>} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
+import { data } from '../../Data/NavbarData';
 
 //Styled Navbar
-import './Navbar';
+import './NavbarStyle.css';
 
 const Navbar = () => {
+  let navigate = useNavigate();
+  let location = useLocation();
+
+
+  const handleClick = () => {
+    navigate("/Home");
+  }
+
   return (
-    <div>Navbar</div>
+    <div>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/group"> Group </Link>
+        <Link to="/contact"> Contact </Link>
+      </nav>
+    </div>
   )
 }
 
