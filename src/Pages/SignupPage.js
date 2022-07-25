dae761ff6c03e80b097b22425f400042096205e4
Marcos Gabriel Martinez <gabriel_martinez_2409@hotmail.es>
2022-07-25 09:39:06 -0300

    Funcionamiento Firebase Login

--- src/Pages/SignupPage.js
+++ src/Pages/SignupPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signOut,
+	signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '../Data/firebaseConfig';
 
 //Bootstrap components
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -14,10 +20,53 @@ const SignupPage = () => {
 	const [registerPassword, setRegisterPassword] = useState('');
 	const [loginEmail, setLoginEmail] = useState('');
 	const [loginPassword, setLoginPassword] = useState('');
+	//---------------------------------
+	//OBTENER USUARIO CON SESION INICIADA
+	//---------------------------------
+	/*const [user, setUser] = useState({});
+	onAuthStateChanged(auth, (currentUser) => {
+		setUser(currentUser);
+	});*/
+	const user = auth.currentUser;
 
-	const register = async () => {};
-	const login = async () => {};
-	const logout = async () => {};
+	//---------------------------------
+	//REGISTRAR a un usuario
+	//---------------------------------
+	const register = async () => {
+		try {
+			const user = await createUserWithEmailAndPassword(
+				auth,
+				registerEmail,
+				registerPassword
+			);
+			console.log(user);
+		} catch (error) {
+			console.log(error.message);
+			//const errorCode = error.code;
+			//const errorMessage = error.message;
+		}
+	};
+	//---------------------------------
+	//Conectarse a un usuario
+	//---------------------------------
+	const login = async () => {
+		try {
+			const user = await signInWithEmailAndPassword(
+				auth,
+				loginEmail,
+				loginPassword
+			);
+			console.log(user);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+	//---------------------------------
+	//DESLOGUEAR a un usuario
+	//---------------------------------
+	const logout = async () => {
+		await signOut(auth);
+	};
 
 	return (
 		<div>
@@ -28,31 +77,35 @@ const SignupPage = () => {
 						<Card style={{ width: '50rem' }}>
 							<Card.Body>
 								<Card.Title>SignUp - Crear usuario</Card.Title>
-								<Form>
-									<Row>
-										<Col>
-											<Form.Group className="mb-3" controlId="formBasicEmail">
-												<Form.Label>Email address</Form.Label>
-												<Form.Control type="email" placeholder="Enter email" />
-												<Form.Text className="text-muted">
-													We'll never share your email with anyone else.
-												</Form.Text>
-											</Form.Group>
-										</Col>
-										<Col>
-											<Form.Group
-												className="mb-3"
-												controlId="formBasicPassword"
-											>
-												<Form.Label>Password</Form.Label>
-												<Form.Control type="password" placeholder="Password" />
-											</Form.Group>
-										</Col>
-									</Row>
-									<Button variant="primary" type="submit">
-										Create
-									</Button>
-								</Form>
+
+								<Row>
+									<Col>
+										<h6>Email address: </h6>
+										<input
+											type="email"
+											placeholder="Enter email"
+											onChange={(event) => {
+												setRegisterEmail(event.target.value);
+											}}
+										/>
+										<p className="text-muted">
+											We'll never share your email with anyone else.
+										</p>
+									</Col>
+									<Col>
+										<h6>Password:</h6>
+										<input
+											type="password"
+											placeholder="Password"
+											onChange={(event) => {
+												setRegisterPassword(event.target.value);
+											}}
+										/>
+									</Col>
+								</Row>
+								<Button variant="primary" onClick={register}>
+									Create
+								</Button>
 							</Card.Body>
 						</Card>
 					</Col>
@@ -63,31 +116,35 @@ const SignupPage = () => {
 						<Card style={{ width: '50rem' }}>
 							<Card.Body>
 								<Card.Title>Login</Card.Title>
-								<Form>
-									<Row>
-										<Col>
-											<Form.Group className="mb-3" controlId="formBasicEmail">
-												<Form.Label>Email address</Form.Label>
-												<Form.Control type="email" placeholder="Enter email" />
-												<Form.Text className="text-muted">
-													We'll never share your email with anyone else.
-												</Form.Text>
-											</Form.Group>
-										</Col>
-										<Col>
-											<Form.Group
-												className="mb-3"
-												controlId="formBasicPassword"
-											>
-												<Form.Label>Password</Form.Label>
-												<Form.Control type="password" placeholder="Password" />
-											</Form.Group>
-										</Col>
-									</Row>
-									<Button variant="primary" type="submit">
-										Login
-									</Button>
-								</Form>
+
+								<Row>
+									<Col>
+										<h6>Email address</h6>
+										<input
+											type="email"
+											placeholder="Enter email"
+											onChange={(event) => {
+												setLoginEmail(event.target.value);
+											}}
+										/>
+										<p className="text-muted">
+											We'll never share your email with anyone else.
+										</p>
+									</Col>
+									<Col>
+										<h6>Password</h6>
+										<input
+											type="password"
+											placeholder="Password"
+											onChange={(event) => {
+												setLoginPassword(event.target.value);
+											}}
+										/>
+									</Col>
+								</Row>
+								<Button variant="primary" onClick={login}>
+									Login
+								</Button>
 							</Card.Body>
 						</Card>
 					</Col>
@@ -100,10 +157,15 @@ const SignupPage = () => {
 								<Card.Title>User - Logout</Card.Title>
 								<Row>
 									<Col>
-										<p>Usuario Logueado: </p>
+										<h6>User logged In:</h6>
+										{user !== null ? user.email : ''}
 									</Col>
 									<Col>
-										<Button variant="primary" type="submit">
+										<Button
+											variant="primary"
+											onClick={logout}
+											disabled={user !== null ? false : true}
+										>
 											Logout
 										</Button>
 									</Col>
