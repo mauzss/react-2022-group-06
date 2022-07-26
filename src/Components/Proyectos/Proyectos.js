080e0bdb412e51bbaced4b3ab0f4044199ea5ddf
Marcos Gabriel Martinez <gabriel_martinez_2409@hotmail.es>
2022-07-26 10:34:30 -0300

    adicion de una carta en proyectos

--- src/Components/Proyectos/Proyectos.js
+++ src/Components/Proyectos/Proyectos.js
@@ -25,7 +25,10 @@ const Proyectos = () => {
 				</Row>
 				<Row>
 					<Col>
-						<Card className="card text-white bg-dark mb-3 card-black" style={{ width: '18rem' }}>
+						<Card
+							className="card text-white bg-dark mb-3 card-black"
+							style={{ width: '18rem' }}
+						>
 							<Card.Img variant="top" src="portafolio-mgm.netlify.app.png" />
 							<Card.Body>
 								<Card.Title>Portfolio</Card.Title>
@@ -39,19 +42,42 @@ const Proyectos = () => {
 						</Card>
 					</Col>
 					<Col>
-						<Card className="card text-white bg-dark mb-3 card-black" style={{ width: '18rem' }}>
+						<Card
+							className="card text-white bg-dark mb-3 card-black"
+							style={{ width: '18rem' }}
+						>
 							<Card.Img variant="top" src="sello-mistico.png" />
 							<Card.Body>
 								<Card.Title>Luna Magica</Card.Title>
 								<Card.Text>
 									Lectura de cartas del tarot - trabajos de Codo a Codo 4.0.
 								</Card.Text>
-								<Card.Link href="https://portafolio-mgm.netlify.app">
+								<Card.Link href="https://luna-magica-astral.netlify.app">
 									Link Luna Magica Astral
 								</Card.Link>
 							</Card.Body>
 						</Card>
 					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<Card
+							className="card text-white bg-dark mb-3 card-black"
+							style={{ width: '18rem' }}
+						>
+							<Card.Img variant="top" src="areacircle-rectangle.jpg" />
+							<Card.Body>
+								<Card.Title>Calculadora de Area</Card.Title>
+								<Card.Text>
+									Calculadora de Area de un circulo y un triangulo reactangulo
+								</Card.Text>
+								<Card.Link href="https://desafios-martinez-marcos-gabriel.netlify.app/desafio2.html">
+									Link Portfolio CSS
+								</Card.Link>
+							</Card.Body>
+						</Card>
+					</Col>
+					<Col></Col>
 				</Row>
 			</div>
 		</div>
